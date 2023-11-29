c01fd4f1bd2581edd8766057d7c8b2be1d013a48
VasylStepanov <vasylstepanov.v@gmail.com>
2023-11-29 19:06:26 +0300

    Added Redis test configuration file.

---
+++ src/test/java/com/application/configuration/TestRedisConfiguration.java
@@ -0,0 +1,26 @@
+package com.application.configuration;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.boot.test.context.TestConfiguration;
+import redis.embedded.RedisServer;
+
+@TestConfiguration
+public class TestRedisConfiguration {
+    private RedisServer redisServer;
+
+    public TestRedisConfiguration(){
+
+        redisServer = new RedisServer(6379);
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        redisServer.stop();
+    }
+}
