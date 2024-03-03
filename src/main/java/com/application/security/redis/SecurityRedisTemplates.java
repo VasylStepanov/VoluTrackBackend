68831b8396f2123c170aaea7b4baab63aca3f519
VasylStepanov <vasylstepanov.v@gmail.com>
2024-03-04 01:37:34 +0300

    Cleanup imports and repair README file.

--- src/main/java/com/application/security/redis/SecurityRedisTemplates.java
+++ src/main/java/com/application/security/redis/SecurityRedisTemplates.java
@@ -1,6 +1,5 @@
 package com.application.security.redis;
 
-import com.application.security.token.DisabledToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
